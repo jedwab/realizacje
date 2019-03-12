37b6493115b78871439ed4317df39ed0678f64a2
Justyna Bobrowska <justynabobrowska88@gmail.com>
2019-03-12 07:46:17 +0100

    newt version

---
+++ js/scroll_reveal.js
@@ -0,0 +1,11 @@
+window.sr = ScrollReveal();
+		sr.reveal('.icon, .icon_contact', {
+			reset: true,
+			delay: 300,
+			distance: 0
+		});
+		sr.reveal('h1, h2, h3, p ', {
+			reset: true,
+			delay: 300
+		});	
+	
---
+++ js/toggler.js
@@ -0,0 +1,5 @@
+(function($) {
+    $("#toggle").click(function() {
+      $('body').toggleClass("open-menu");
+    });
+  })(jQuery);
